03d67e433c2bc57382fce9a9524e9f27810b53ca
SoyKevin111 <kevinstevennn768xd@gmail.com>
2025-12-26 08:56:42 -0500

    prueba migraciones, get, put, post, delete

--- projectef/Program.cs
+++ projectef/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using projectef;
+using projectef.src.models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,9 +31,48 @@ app.MapGet("/dbconnection", async ([FromServices] TasksContext dbContext) =>
   }
 });
 
-app.MapGet("/api/tareas", async ([FromServices] TasksContext dbContext) =>
+app.MapGet("/api/tasks", async ([FromServices] TasksContext dbContext) =>
 {
-  return Results.Ok(dbContext.Task.Include(t => t.category).Where(t => t.PriorityTask == projectef.src.models.Priority.Medium));
+  return Results.Ok(dbContext.Task.Include(t => t.category));
+});
+
+app.MapPost("/api/tasks", async ([FromServices] TasksContext dbContext, [FromBody] TodoTask task) =>
+{
+  task.TaskId = Guid.NewGuid();
+  task.Create_on = DateTime.UtcNow; //zona horaria local global
+  await dbContext.Task.AddAsync(task);
+  await dbContext.SaveChangesAsync();
+  return Results.Ok();
+});
+
+app.MapPut("/api/tasks/{id}", async ([FromServices] TasksContext dbContext, [FromBody] TodoTask task, [FromRoute] Guid id) =>
+{
+
+  //buscar tarea actual
+  var currentTask = dbContext.Task.Find(id);
+  if (currentTask == null)
+  {
+    return Results.NotFound();
+  }
+  currentTask.CategoryId = task.CategoryId;
+  currentTask.Title = task.Title;
+  currentTask.PriorityTask = task.PriorityTask;
+  currentTask.Description = task.Description;
+
+  await dbContext.SaveChangesAsync();
+  return Results.Ok();
+});
+
+app.MapDelete("/api/tasks/{id}", async ([FromServices] TasksContext dbContext, [FromRoute] Guid id) =>
+{
+  var currentTask = dbContext.Task.Find(id);
+  if (currentTask != null)
+  {
+    dbContext.Remove(currentTask);
+    await dbContext.SaveChangesAsync();
+    return Results.Ok();
+  }
+  return Results.NotFound();
 });
 
 
