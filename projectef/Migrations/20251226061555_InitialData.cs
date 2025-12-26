9f6e567cec82b201f915aa53f5b464fc52b8a479
SoyKevin111 <kevinstevennn768xd@gmail.com>
2025-12-26 01:18:17 -0500

    prueba migraciones, datos iniciales

---
+++ projectef/Migrations/20251226061555_InitialData.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace projectef.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "task",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Tag",
+                table: "category",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "category",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.InsertData(
+                table: "category",
+                columns: new[] { "CategoryId", "Descripcion", "Name", "Peso", "Tag" },
+                values: new object[,]
+                {
+                    { new Guid("baed9063-a68c-4e94-8f55-19c2fc68f752"), "lorem ipsun door af terque cua fofvy ether who", "Actividades Personales", 70, null },
+                    { new Guid("baed9063-a68c-4e94-8f55-19c2fc68f75b"), "lorem ipsun door af terque cua fofvy ether who", "Actividades Pendientes", 60, null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "task",
+                columns: new[] { "TaskId", "CategoryId", "Create_on", "Description", "PriorityTask", "Title" },
+                values: new object[,]
+                {
+                    { new Guid("baed9063-a68c-4e94-8f55-19c2fc68f751"), new Guid("baed9063-a68c-4e94-8f55-19c2fc68f75b"), new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), null, 1, "Pago de servicios Publicos" },
+                    { new Guid("baed9063-a68c-4e94-8f55-19c2fc68f759"), new Guid("baed9063-a68c-4e94-8f55-19c2fc68f752"), new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), null, 2, "Pago de servicios Privados" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "task",
+                keyColumn: "TaskId",
+                keyValue: new Guid("baed9063-a68c-4e94-8f55-19c2fc68f751"));
+
+            migrationBuilder.DeleteData(
+                table: "task",
+                keyColumn: "TaskId",
+                keyValue: new Guid("baed9063-a68c-4e94-8f55-19c2fc68f759"));
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "CategoryId",
+                keyValue: new Guid("baed9063-a68c-4e94-8f55-19c2fc68f752"));
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "CategoryId",
+                keyValue: new Guid("baed9063-a68c-4e94-8f55-19c2fc68f75b"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "task",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Tag",
+                table: "category",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "category",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
--- projectef/TasksContext.cs
+++ projectef/TasksContext.cs
@@ -11,14 +11,55 @@ public class TasksContext : DbContext
 
   protected override void OnModelCreating(ModelBuilder modelBuilder)
   {
+
+    List<Category> categoriesInit =
+    [
+      new Category()
+      {
+        CategoryId = Guid.Parse("baed9063-a68c-4e94-8f55-19c2fc68f75b"),
+        Name = "Actividades Pendientes",
+        Descripcion = "lorem ipsun door af terque cua fofvy ether who",
+        Peso = int.Parse("60")
+      },
+      new Category()
+      {
+        CategoryId = Guid.Parse("baed9063-a68c-4e94-8f55-19c2fc68f752"),
+        Name = "Actividades Personales",
+        Descripcion = "lorem ipsun door af terque cua fofvy ether who",
+        Peso = int.Parse("70")
+      },
+    ];
+
+    List<TodoTask> tasksInit =
+    [
+      new TodoTask()
+      {
+        TaskId = Guid.Parse("baed9063-a68c-4e94-8f55-19c2fc68f751"),
+        CategoryId = Guid.Parse("baed9063-a68c-4e94-8f55-19c2fc68f75b"),
+        PriorityTask = Priority.Medium,
+        Title = "Pago de servicios Publicos",
+        Create_on = new DateTime(2025, 01, 01, 0, 0, 0, DateTimeKind.Utc)
+      },
+      new TodoTask()
+      {
+        TaskId = Guid.Parse("baed9063-a68c-4e94-8f55-19c2fc68f759"),
+        CategoryId = Guid.Parse("baed9063-a68c-4e94-8f55-19c2fc68f752"),
+        PriorityTask = Priority.high,
+        Title = "Pago de servicios Privados",
+        Create_on = new DateTime(2025, 01, 01, 0, 0, 0, DateTimeKind.Utc)
+      }
+    ];
+
     modelBuilder.Entity<Category>(category =>
     {
       category.ToTable("category");
       category.HasKey(c => c.CategoryId);
       category.Property(c => c.Name).IsRequired().HasMaxLength(150);
-      category.Property(c => c.Descripcion);
+      category.Property(c => c.Descripcion).IsRequired(false);
       category.Property(c => c.Peso);
-      category.Property(c => c.Tag);
+      category.Property(c => c.Tag).IsRequired(false); ;
+
+      category.HasData(categoriesInit);
     });
 
     modelBuilder.Entity<TodoTask>(task =>
@@ -28,10 +69,12 @@ public class TasksContext : DbContext
       //t.category | c.tasks -> foreignKey 
       task.HasOne(t => t.category).WithMany(c => c.Tasks).HasForeignKey(t => t.CategoryId);//categoryId
       task.Property(t => t.Title).IsRequired().HasMaxLength(200);
-      task.Property(t => t.Description);
+      task.Property(t => t.Description).IsRequired(false);
       task.Property(t => t.PriorityTask);
       task.Property(t => t.Create_on);
       task.Ignore(t => t.Summary); //no mapear
+
+      task.HasData(tasksInit);
 
     });
   }
